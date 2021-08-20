b0827fee0efc4e9879ad4352b35a2b1c98269f2e
okayu8 <hitugaya12@gmail.com>
2021-08-21 01:26:40 +0900

    initial commit

--- CameraApp/ViewController.swift
+++ CameraApp/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +15,35 @@ class ViewController: UIViewController {
     }
 
 
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    // カメラ起動
+    @IBAction func cameraLaunchAction(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera can be used.")
+            let ipc = UIImagePickerController()
+            ipc.sourceType = .camera
+            ipc.delegate = self
+            present(ipc, animated: true, completion: nil)
+        } else {
+            print("Camera is not avairable")
+        }
+    }
+    
+    // SNSシェア
+    @IBAction func shareAction(_ sender: Any) {
+        if let sharedImage = photoImage.image {
+            let sharedItems = [sharedImage]
+            let controller = UIActivityViewController(activityItems: sharedItems, applicationActivities: nil)
+            controller.popoverPresentationController?.sourceView = view
+            present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    // 画像を取得する
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        photoImage.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
 }
 
